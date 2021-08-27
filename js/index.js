ad71d3080658dcd181963340bbbbaac43789f386
Matheus Henrique Santos Da Silva <28486@eurofarma.com.br>
2021-08-27 17:41:20 -0300

    Create Portfolio project

---
+++ js/index.js
@@ -0,0 +1,27 @@
+/*============== MENU SHOW E HIDDEN ================*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+/*============== MENU SHOW ================*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*==================== Remove Menu MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
