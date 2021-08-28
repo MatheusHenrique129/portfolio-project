7867c0a8aee52f4567c2aff221f65163ea3b237e
Matheus Henrique Santos Da Silva <28486@eurofarma.com.br>
2021-08-28 16:13:57 -0300

    Desenvovendo frente do Portfolio

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /*============== MENU SHOW E HIDDEN ================*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu")
+const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
 
 /*============== MENU SHOW ================*/
@@ -17,7 +17,7 @@ if (navClose) {
 }
 
 /*==================== Remove Menu MOBILE ====================*/
-const navLink = document.querySelectorAll(".nav__link");
+const navLink = document.querySelectorAll(".nav-link");
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
@@ -25,3 +25,66 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+  let itemClass = this.parentNode.className;
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills-close';
+  }
+  if(itemClass === 'skills__content skills-close'){
+     this.parentNode.className = 'skills__content skills-open';
+  }
+}
+
+skillsHeader.forEach((e) => {
+  e.addEventListener('click', toggleSkills);
+})
+
+
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () =>{
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent =>{
+      tabContent.classList.remove('qualification-active')
+    })
+    target.classList.add('qualification-active')
+
+    tab.forEach(tab =>{
+      tab.classList.remove('qualification-active')
+    })
+    tab.classList.add('qualification-active')
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
