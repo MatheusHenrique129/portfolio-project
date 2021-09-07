80760f41771687831cb71420fc5e677baa5bd9d0
Matheus Henrique Santos Da Silva <28486@eurofarma.com.br>
2021-09-07 00:25:36 -0300

    1.0 Version

--- js/index.js
+++ js/index.js
@@ -68,23 +68,126 @@ tabs.forEach(tab => {
   })
 })
 
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services-modal');
+const modalBtns = document.querySelectorAll('.services-button');
+const modalClose = document.querySelectorAll('.services-modal-close');
 
 
+let modal = function(modalClick){
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalClose.forEach((modalClose) => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal')
+    })
+  })
+})
+
+/*==================== PORTFOLIO SWIPER ====================*/
+let swiperPortfolio = new Swiper(".portfolio-container", {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+/*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial-container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints:{
+    568:{
+      slidesPerView: 2,
+    },
+  }
+});
 
 
+/*==================== Scroll Sections Link ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(e =>{
+    const sectionHeight = e.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    secti
+  })
+}
 
 
+/*==================== Scroll Header Link ====================*/
 
 
+/*==================== Scroll UP SHoW ==================*/
 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
 
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll');
 
+  else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
+/*==================== DARK THEME ==================*/
 
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
 
-
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 
